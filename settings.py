12a59f9c6a54a8b2b94e736232acaed20f2f4268
Julian Aparicio <japaric4@uci.edu>
2023-09-27 11:09:51 -0700

    I changed the path of the base dir back to how it was but took off one of the parents

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "/Users/we3kends0onlyy/Documents/workout-project/workouts2.db",
+        "NAME": BASE_DIR / "workouts2.db",
     }
 }
 
