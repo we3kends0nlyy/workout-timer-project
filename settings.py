40220457a04092acaad9cb488562184c61597aa5
Julian Aparicio <japaric4@uci.edu>
2023-09-27 11:10:40 -0700

    changed it one more time

--- settings.py
+++ settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR2 = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -77,7 +78,7 @@ WSGI_APPLICATION = "wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "workouts2.db",
+        "NAME": BASE_DIR2 / "workouts2.db",
     }
 }
 
